10ce8c94f40ee53832a5ea35b3544f84e18510b5
Raymond Willey <raymond_willey@me.com>
2018-10-14 16:04:57 -0400

    Added fetchall()

--- sql_selects.py
+++ sql_selects.py
@@ -2,19 +2,19 @@ def select_all_columns_and_rows():
     return cursor.execute('''SELECT * FROM planets;''').fetchall()
 
 def select_name_and_color_of_all_planets():
-    return cursor.execute('''SELECT name, color FROM planets;''')
+    return cursor.execute('''SELECT name, color FROM planets;''').fetchall()
 
 def select_all_planets_with_mass_greater_than_one():
-    return cursor.execute('''SELECT * FROM planets WHERE mass > 1;''')
+    return cursor.execute('''SELECT * FROM planets WHERE mass > 1;''').fetchall()
 
 def select_name_and_mass_of_planets_with_mass_less_than_equal_to_one():
-    return cursor.execute('''SELECT name, mass FROM planets WHERE mass >= 1;''')
+    return cursor.execute('''SELECT name, mass FROM planets WHERE mass >= 1;''').fetchall()
 
 def select_name_and_color_of_planets_with_more_than_10_moons():
-    return cursor.execute('''SELECT name, color FROM planets WHERE num_of_moons > 10;''')
+    return cursor.execute('''SELECT name, color FROM planets WHERE num_of_moons > 10;''').fetchall()
 
 def select_all_planets_with_moons_and_mass_less_than_one():
-    return cursor.execute('''SELECT name, color FROM planets WHERE num_of_moons >= 1 AND mass < 1;''')
+    return cursor.execute('''SELECT name, color FROM planets WHERE num_of_moons >= 1 AND mass < 1;''').fetchall()
 
 def select_name_and_color_of_all_blue_planets():
-    return cursor.execute('''SELECT name, color FROM planets WHERE color LIKE '%blue%';''')
+    return cursor.execute('''SELECT name, color FROM planets WHERE color LIKE '%blue%';''').fetchall()
