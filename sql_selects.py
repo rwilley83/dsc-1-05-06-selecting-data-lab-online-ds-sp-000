6554f5964ae0d8bc7be1ef038d674e6b8c43c431
Raymond Willey <raymond_willey@me.com>
2018-10-14 15:21:35 -0400

    Lab complete, unable to run tests. Required files not found.

--- sql_selects.py
+++ sql_selects.py
@@ -1,20 +1,20 @@
 def select_all_columns_and_rows():
-    return ""
+    return cursor.execute('''SELECT * FROM planets;''').fetchall()
 
 def select_name_and_color_of_all_planets():
-    return ""
+    return cursor.execute('''SELECT name, color FROM planets;''')
 
 def select_all_planets_with_mass_greater_than_one():
-    return ""
+    return cursor.execute('''SELECT * FROM planets WHERE mass > 1;''')
 
 def select_name_and_mass_of_planets_with_mass_less_than_equal_to_one():
-    return ""
+    return cursor.execute('''SELECT name, mass FROM planets WHERE mass >= 1;''')
 
 def select_name_and_color_of_planets_with_more_than_10_moons():
-    return ""
+    return cursor.execute('''SELECT name, color FROM planets WHERE num_of_moons > 10;''')
 
 def select_all_planets_with_moons_and_mass_less_than_one():
-    return ""
+    return cursor.execute('''SELECT name, color FROM planets WHERE num_of_moons >= 1 AND mass < 1;''')
 
 def select_name_and_color_of_all_blue_planets():
-    return ""
+    return cursor.execute('''SELECT name, color FROM planets WHERE color LIKE '%blue%';''')
